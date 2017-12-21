e1aedd305405c1b2206c782f0e618018cf449b6d
Michael Blankenship <Blankenship@OriginCodeAcademy.com>
2017-12-21 10:28:28 -0800

    v1.0.1 HelloSwift

---
+++ HelloSwift/HelloSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HelloSwift
+//
+//  Created by Justin Case on 12/21/17.
+//  Copyright © 2017 OutsourcedGuru. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, Swift (OS X)")
+
